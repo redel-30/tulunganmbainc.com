51d911486a28a54a87c63feb9fac17856dfc2e0d
redel-30 <redelsantiago150@gmail.com>
2021-08-10 09:36:30 +0800

    add all

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.slider').slider({
+		height: 400,
+		indicators: true,
+	}
+	);
+	$(".dropdown-trigger").dropdown({
+		constrainWidth: false,
+	});
+	$('.materialboxed').materialbox();
+})
